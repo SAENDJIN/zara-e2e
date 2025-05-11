a9cbbb0765823459606b17def13c8bffd644e498
andrii.sa <andrii.sa@oxoix.com>
2025-05-11 12:58:30 +0300

    fix double click

--- app/pages/RegistrationPage.ts
+++ app/pages/RegistrationPage.ts
@@ -29,10 +29,12 @@ export class RegistrationPage extends BasePage {
         await this.passwordInput.fill('123qweASD');
         await this.nameInput.fill('test');
         await this.surnameInput.fill('test');
+        await this.surnameInput.blur();
+
         //IDK or understand why i need to click twice on a checkbox.
         //I've spent over three hours here -_-
         await this.privacyCheckBox.click();
-        await this.privacyCheckBox.click();
+        // await this.privacyCheckBox.click();
         await this.createAccountBtn.click();
     }
     async registerFailCheck() {
