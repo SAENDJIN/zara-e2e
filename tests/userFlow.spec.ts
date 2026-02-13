ce6827004fbb5d209d32a1e14e41bd42456de9be
andrii.sa <andrii.sa@oxoix.com>
2026-02-13 13:29:10 +0200

    skip test

--- tests/userFlow.spec.ts
+++ tests/userFlow.spec.ts
@@ -4,8 +4,8 @@ import { argosScreenshot } from "@argos-ci/playwright";
 
 test('ZA-1 Full cart flow and invalid registration validation', async({page, searchComponents, cookieModal, productPage, cartPage, registrationPage })=> {
     await cookieModal.goto();
-    await cookieModal.btnsVisible();
     await argosScreenshot(page, "homepage");
+    await cookieModal.btnsVisible();
     await cookieModal.acceptAllCookies();
     await cookieModal.cookiesModalHidden();
 
@@ -13,12 +13,12 @@ test('ZA-1 Full cart flow and invalid registration validation', async({page, sea
     await argosScreenshot(page, "search");
     await searchComponents.clickOnProductWithFourOrMoreSizes();
 
-    await productPage.addAllAvailableSizes();
-    await productPage.openCart();
+    // await productPage.addAllAvailableSizes();
+    // await productPage.openCart();
 
-    await cartPage.deleteEverySecondItem();
-    await cartPage.proceedToCheckout();
+    // await cartPage.deleteEverySecondItem();
+    // await cartPage.proceedToCheckout();
 
-    await registrationPage.registerNewUser();
-    await registrationPage.registerFailCheck();
+    // await registrationPage.registerNewUser();
+    // await registrationPage.registerFailCheck();
 })
